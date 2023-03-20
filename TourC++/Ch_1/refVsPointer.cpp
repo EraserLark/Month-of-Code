cda3120336fdd7fe284851f1dde69a335969872c
Conner A <eraserlark@gmail.com>
2023-03-20 14:33:19 -0500

    Reference vs pointer

---
+++ TourC++/Ch_1/refVsPointer.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+void pointerPrint(int *arrayPtr);
+void referencePrint(int& arrayRef);
+
+int main()
+{
+    int v[] {0,1,2,3,4};
+
+    //Copy array
+    for(int i : v)
+    {
+        std::cout << "With Copying:" << i << '\n';
+    }
+
+    pointerPrint(v);
+    referencePrint(*v);
+}
+
+void pointerPrint(int *arrayPtr)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << "With Pointer:" << arrayPtr[i] << '\n';
+    } 
+}
+
+void referencePrint(int& arrayRef)
+{
+    for(int i = 0; i < 5; i++)
+    {
+        std::cout << "With Reference:" << arrayRef << '\n';
+    }
+}
