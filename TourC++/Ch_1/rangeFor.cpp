466f93acae6a3b4524ce39f825c847c563bcb171
Conner A <eraserlark@gmail.com>
2023-03-20 13:56:35 -0500

    Print v

--- TourC++/Ch_1/rangeFor.cpp
+++ TourC++/Ch_1/rangeFor.cpp
@@ -4,9 +4,11 @@ int main()
 {
     int v[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+    int i = 0;
     for(int& x : v)
     {
         ++x;
-        std::cout << x << '\n';
+        std::cout << v[i] << '\n';
+        i++;
     }
 }
