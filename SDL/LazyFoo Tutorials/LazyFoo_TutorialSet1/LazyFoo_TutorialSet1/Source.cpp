072cbaed9d10893272cd132c9da60f753a36a3e5
Conner A <eraserlark@gmail.com>
2023-03-21 15:44:38 -0500

    Lesson2_ImageOnScreen

--- SDL/LazyFoo Tutorials/LazyFoo_TutorialSet1/LazyFoo_TutorialSet1/Source.cpp
+++ SDL/LazyFoo Tutorials/LazyFoo_TutorialSet1/LazyFoo_TutorialSet1/Source.cpp
@@ -24,12 +24,66 @@ bool init()
 	else
 	{
 		gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-			//Left off here!!!
+		if (gWindow == NULL)
+		{
+			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
+		}
+		else
+		{
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
+		}
 	}
+
+	return success;
+}
+
+bool loadMedia()
+{
+	bool success = true;
+
+	gHelloWorld = SDL_LoadBMP("projectMaterials/T2_ImageOnScreen/bmp_24.bmp");
+	if (gHelloWorld == NULL)
+	{
+		printf("Unable to load image %s! SDL Error: %s\n", "projectMaterials/T2_ImageOnScreen/bmp_24.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void close()
+{
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
 }
 
 int main(int argc, char* argv[])
 {
+	if (!init())
+	{
+		printf("Failed to initialize\n");
+	}
+	else
+	{
+		if (!loadMedia())
+		{
+			printf("Failed to load media\n");
+		}
+		else
+		{
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL );
+			SDL_UpdateWindowSurface(gWindow);
+			SDL_Event e; bool quit = false; while (quit == false) { while (SDL_PollEvent(&e)) { if (e.type == SDL_QUIT) quit = true; } }	//Hack
+		}
+	}
+
+	close();
 
 	return 0;
 }
