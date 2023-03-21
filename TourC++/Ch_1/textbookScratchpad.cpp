2f2aeef7e73784570aeea72d59cb3e0d6897a43f
Conner A <eraserlark@gmail.com>
2023-03-21 10:24:02 -0500

    move file content

--- TourC++/Ch_1/textbookScratchpad.cpp
+++ TourC++/Ch_1/textbookScratchpad.cpp
@@ -4,16 +4,5 @@ using namespace std;
 
 int main()
 {
-    cout << "Enter a number: ";
-    int i;
-    cin >> i;
-
-    if(int n = i)   //create local var 'n', n gets i. Enter if n != 0
-    {
-        cout << "The number is NOT 0 -- n = " << n << '\n';
-    }
-    else
-    {
-        cout << "The number is 0 -- n = " << n << '\n';
-    }
+    
 }
