f4be4548f8496c6a0c8eceb1c7046732bef671e1
Conner A <eraserlark@gmail.com>
2023-03-22 09:38:28 -0500

    cleanup

--- TourC++/Ch_3/scratchpad.cpp
+++ TourC++/Ch_3/scratchpad.cpp
@@ -1,31 +1,5 @@
 #include <iostream>
 
-class Vector{
-    public:
-        Vector(int s);
-        double& operator[](int i);
-        int size();
-    private:
-        double* elem;
-        int sz;
-};
-
-Vector::Vector(int s)
-{
-    elem = new double[s];
-    sz = s;
-}
-
-double& Vector::operator[](int i)
-{
-    return elem[i];
-}
-
-int Vector::size()
-{
-    return sz;
-}
-
 int main()
 {
 
