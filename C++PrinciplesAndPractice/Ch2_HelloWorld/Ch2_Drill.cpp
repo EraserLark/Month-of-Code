54eb3978f120e80c08b232eddd0406c4cbfac096
Conner A <eraserlark@gmail.com>
2023-03-23 16:29:46 -0500

    P&P_Ch2

---
+++ C++PrinciplesAndPractice/Ch2_HelloWorld/Ch2_Drill.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+inline void keep_window_open() {char ch; cin>>ch;}
+
+int main()
+{
+    cout << "Hello, World\n";
+    keep_window_open();
+    return 0;
+}
--- TourC++/Ch_4/scratchpad.cpp
+++ TourC++/Ch_4/scratchpad.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+
+std::vector<int> v1;
+std::vector<int> v2 {v1};
 
 int main()
 {
