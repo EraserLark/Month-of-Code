749a1994506b45ed8ae4d1a166cfc7ac11b3180f
Conner A <eraserlark@gmail.com>
2023-03-24 10:09:16 -0500

    conversion to months

--- C++PrinciplesAndPractice/Ch3_Objects/try1_NameAndAge.cpp
+++ C++PrinciplesAndPractice/Ch3_Objects/try1_NameAndAge.cpp
@@ -3,10 +3,12 @@
 int main()
 {
     std::string first;
-    int age;
+    double age;
 
     std::cout << "Please enter your name and age\n";
     std::cin >> first >> age;
-    
-    std::cout << "Hello " << first <<" (age: "<< age <<")\n";
+
+    double months = age * 12;
+
+    std::cout << "Hello " << first <<" (age: "<< months <<" months)\n";
 }
