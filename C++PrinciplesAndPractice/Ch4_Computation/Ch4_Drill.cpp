f1ae8f52de241690e4debfb3613aeabf4be71796
Conner A <eraserlark@gmail.com>
2023-03-24 18:26:16 -0500

    Revert "1-3 complete"
    This reverts commit 835f30aefcff77b8f1194d61eb8c8c038b27a870.

--- C++PrinciplesAndPractice/Ch4_Computation/Ch4_Drill.cpp
+++ C++PrinciplesAndPractice/Ch4_Computation/Ch4_Drill.cpp
@@ -7,27 +7,8 @@ int main()
 
     while(std::cin >> a, std::cin >> b)
     {
-        int small;
-        int large;
-
-        if(a > b)
-        {
-            large = a;
-            small = b;
-        }
-        else if(a < b)
-        {
-            large = b;
-            small = a;
-        }
-        else if (a == b)
-        {
-            std::cout << "The numbers are equal!";
-            return 0;
-        }
-
-        std::cout << "The smaller value is: " << small << '\n';
-        std::cout << "The larger value is: " << large << '\n';
+        std::cout << a;
+        std::cout << b;
         std::cout << std::endl;
     }
 }
