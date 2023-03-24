93cfad73ca1f729c41d72d1daaea44b11a0a20d5
Conner A <eraserlark@gmail.com>
2023-03-24 15:40:29 -0500

    Ch4 Drill

---
+++ C++PrinciplesAndPractice/Ch4_Computation/Ch4_Drill.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+
+    while(std::cin >> a, std::cin >> b)
+    {
+        std::cout << a;
+        std::cout << b;
+        std::cout << std::endl;
+    }
+}
