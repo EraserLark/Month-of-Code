8b200e468a0ba5132ffc39f8e80d261ac918ca82
Conner A <eraserlark@gmail.com>
2023-03-24 11:39:22 -0500

    add friend

--- C++PrinciplesAndPractice/Ch3_Objects/Ch3_Drill.cpp
+++ C++PrinciplesAndPractice/Ch3_Objects/Ch3_Drill.cpp
@@ -2,5 +2,34 @@
 
 int main()
 {
-    
+    std::string firstName;
+    std::string friendName;
+    char friendGender = 0;
+
+    std::cout << "Enter recipient name: ";
+    std::cin >> firstName;
+    std::cout << "Enter friend name: ";
+    std::cin >> friendName;
+    std::cout << "Enter friend's gender (m/f/other): ";
+    std::cin >> friendGender;
+
+    std::cout << "Dear " << firstName << ",\n";
+    std::cout << "  How are you? I am fine. I miss you." << std::endl;
+    std::cout << "  Have you seen " << friendName << " lately?\n";
+
+    std::string friendPronouns;
+    switch (friendGender)
+    {
+        case 'm':
+            friendPronouns = "him";
+            break;
+        case 'f':
+            friendPronouns = "her";
+            break;
+        default:
+            friendPronouns = "them";
+            break;
+    }
+
+    std::cout << "  If you see " << friendName << ", please ask " << friendPronouns << " to call me.";
 }
