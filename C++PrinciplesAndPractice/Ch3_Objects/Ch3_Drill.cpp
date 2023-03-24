6a419ca0e9ebc4e8e2565fbba7272aa89b496370
Conner A <eraserlark@gmail.com>
2023-03-24 11:49:43 -0500

    final touches

--- C++PrinciplesAndPractice/Ch3_Objects/Ch3_Drill.cpp
+++ C++PrinciplesAndPractice/Ch3_Objects/Ch3_Drill.cpp
@@ -3,11 +3,18 @@
 int main()
 {
     std::string firstName;
+    int recpAge = 0;
     std::string friendName;
     char friendGender = 0;
 
     std::cout << "Enter recipient name: ";
     std::cin >> firstName;
+    std::cout << "Enter recipient age: ";
+    do
+    {
+        std::cin >> recpAge;
+    } while (recpAge < 0 || recpAge > 110);
+
     std::cout << "Enter friend name: ";
     std::cin >> friendName;
     std::cout << "Enter friend's gender (m/f/other): ";
@@ -31,5 +38,21 @@ int main()
             break;
     }
 
-    std::cout << "  If you see " << friendName << ", please ask " << friendPronouns << " to call me.";
+    std::cout << "  If you see " << friendName << ", please ask " << friendPronouns << " to call me." << std::endl;
+    std::cout << "  I hear you just had a birthday and you're now " << recpAge << " years old!" << std::endl;
+
+    if(recpAge > 70)
+    {
+        std::cout << "  I hope you are enjoying retirement" << std::endl;
+    }
+    else if(recpAge == 17)
+    {
+        std::cout << "  Next year you will be able to vote" << std::endl;
+    }
+    else if(recpAge < 12)
+    {
+        std::cout << "  Next year you will be " << recpAge + 1 << std::endl;
+    }
+
+    std::cout << "Yours sincerely, " << std::endl << std::endl << "Conner";
 }
