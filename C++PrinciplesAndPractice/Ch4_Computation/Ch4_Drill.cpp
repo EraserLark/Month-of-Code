835f30aefcff77b8f1194d61eb8c8c038b27a870
Conner A <eraserlark@gmail.com>
2023-03-24 15:46:27 -0500

    1-3 complete

--- C++PrinciplesAndPractice/Ch4_Computation/Ch4_Drill.cpp
+++ C++PrinciplesAndPractice/Ch4_Computation/Ch4_Drill.cpp
@@ -7,8 +7,27 @@ int main()
 
     while(std::cin >> a, std::cin >> b)
     {
-        std::cout << a;
-        std::cout << b;
+        int small;
+        int large;
+
+        if(a > b)
+        {
+            large = a;
+            small = b;
+        }
+        else if(a < b)
+        {
+            large = b;
+            small = a;
+        }
+        else if (a == b)
+        {
+            std::cout << "The numbers are equal!";
+            return 0;
+        }
+
+        std::cout << "The smaller value is: " << small << '\n';
+        std::cout << "The larger value is: " << large << '\n';
         std::cout << std::endl;
     }
 }
