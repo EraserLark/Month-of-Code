d2334dd5d420aa1016172a27bbfd1205c508d748
Conner A <eraserlark@gmail.com>
2023-03-27 14:33:28 -0500

    Lesson8_GeometryRendering

--- SDL/LazyFoo Tutorials/LazyFoo_TutorialSet1/LazyFoo_TutorialSet1/Source.cpp
+++ SDL/LazyFoo Tutorials/LazyFoo_TutorialSet1/LazyFoo_TutorialSet1/Source.cpp
@@ -87,13 +87,6 @@ bool loadMedia()
 {
 	bool success = true;
 
-	gTexture = loadTexture("projectMaterials/T6_LoadOtherImageFormats/colored.png");
-	if (gTexture == NULL)
-	{
-		printf("Failed to load texture image!\n");
-		success = false;
-	}
-
 	return success;
 }
 
@@ -139,8 +132,26 @@ int main(int argc, char* argv[])
 					}
 				}
 
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
 				SDL_RenderClear(gRenderer);
-				SDL_RenderCopy(gRenderer, gTexture, NULL, NULL);
+
+				SDL_Rect fillRect = { SCREEN_WIDTH / 4, SCREEN_HEIGHT / 4, SCREEN_WIDTH / 2, SCREEN_WIDTH / 2 };
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0x00, 0x00, 0xFF);
+				SDL_RenderFillRect(gRenderer, &fillRect);
+
+				SDL_Rect outlineRect = { SCREEN_WIDTH / 6, SCREEN_HEIGHT / 6, SCREEN_WIDTH * 2 / 3, SCREEN_HEIGHT * 2 / 3 };
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0xFF, 0x00, 0xFF);
+				SDL_RenderDrawRect(gRenderer, &outlineRect);
+
+				SDL_SetRenderDrawColor(gRenderer, 0x00, 0x00, 0xFF, 0xFF);
+				SDL_RenderDrawLine(gRenderer, 0, SCREEN_HEIGHT / 2, SCREEN_WIDTH, SCREEN_HEIGHT / 2);
+
+				SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0x00, 0xFF);
+				for (int i = 0; i < SCREEN_HEIGHT; i+= 4)
+				{
+					SDL_RenderDrawPoint(gRenderer, SCREEN_WIDTH / 2, i);
+				}
+
 				SDL_RenderPresent(gRenderer);
 			}
 		}
