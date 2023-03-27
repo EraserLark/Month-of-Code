b22c0de24a6dffe73850ad7087759fb7053e47ef
Conner A <eraserlark@gmail.com>
2023-03-27 15:34:16 -0500

    basic multiplication

--- C++PrinciplesAndPractice/Ch6_WritingAProgram/calculator.cpp
+++ C++PrinciplesAndPractice/Ch6_WritingAProgram/calculator.cpp
@@ -1,12 +1,37 @@
 #include <iostream>
+#include <vector>
 
 class Token
 {
-    char kind;
-    double value;
+    public:
+        char kind;
+        double value;
 };
+Token getToken();
+
+std::vector<Token> tok;
 
 int main()
+{
+    while(std::cin)
+    {
+        Token t = getToken();
+        tok.push_back(t);
+    }
+
+    for(int i = 0; i < tok.size(); i++)
+    {
+        if(tok[i].kind == '*')
+        {
+            double lVal = tok[i-1].value;
+            double rVal = tok[i+1].value;
+
+            double product = lVal * rVal;
+        }
+    }
+}
+
+Token getToken()
 {
 
 }
