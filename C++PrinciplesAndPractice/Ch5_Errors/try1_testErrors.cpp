122ab23287770ec4401a97cf0bea62d07683cb43
Conner A <eraserlark@gmail.com>
2023-03-27 11:35:35 -0500

    manual error catch

--- C++PrinciplesAndPractice/Ch5_Errors/try1_testErrors.cpp
+++ C++PrinciplesAndPractice/Ch5_Errors/try1_testErrors.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 #include <vector>
 
 using namespace std;
@@ -15,12 +16,24 @@ int main()
 
         for(int i = 0; i <= v.size(); i++)
         {
+            if(i > (v.size() - 1))
+            {
+                throw runtime_error("i is larger than v.size");
+            }
             cout << v[i] << endl;
         }
     }
     catch(out_of_range)
     {
         cerr << "Whoops. Range Error!" << '\n';
+        return 1;
+    }
+    catch(runtime_error)
+    {
+        cerr << "Runtime error" << '\n';
+        return 2;
     }
     
+
+    return 0;
 }
