04801eeef0955caa132f18b3c5bc4a91e5eb1a19
Conner A <eraserlark@gmail.com>
2023-03-28 16:23:18 -0500

    Ch8 Drill

---
+++ C++PrinciplesAndPractice/Ch8_Functions/Drill/my.cpp
@@ -0,0 +1,12 @@
+#include "my.h"
+#include "../../Libraries/std_lib_facilities.h"
+
+void print_foo()
+{
+    cout << foo;
+}
+
+void print(int i)
+{
+    cout << i;
+}
---
+++ C++PrinciplesAndPractice/Ch8_Functions/Drill/use.cpp
@@ -0,0 +1,12 @@
+#include "my.h"
+#include <iostream>
+
+int main()
+{
+    foo = 7;
+    print_foo();
+
+    print(99);
+
+    char cc; std::cin>>cc;
+}
