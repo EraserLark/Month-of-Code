fcae7716f3e0b5185d130047bb79905f895bfb0e
Conner A <eraserlark@gmail.com>
2023-03-29 11:42:15 -0500

    update

--- C++PrinciplesAndPractice/Ch9_Classes/Exercise2.cpp
+++ C++PrinciplesAndPractice/Ch9_Classes/Exercise2.cpp
@@ -2,38 +2,60 @@
 
 class Name_pairs
 {
-    vector<string> name;
-    vector<double> age;
-
-    void read_names(vector<string> names);
-    void read_ages(vector<double> ages);
+    public:
+    void read_names();
+    void read_ages();
 
     void print(const Name_pairs& pairs);
     void sort(Name_pairs& pairs);
+
+    private:
+    vector<string> name;
+    vector<double> age;
 };
 
 int main()
 {
+    Name_pairs n;
+    n.read_names();
+    n.read_ages();
 
     return 0;
 }
 
-void Name_pairs::read_names(vector<string> names)
+void Name_pairs::read_names()
 {
-
+    //Reads a series of names
+    cout << "Enter a series of names: " << endl;
+    
+    string tempName;
+    while(cin >> tempName && tempName != ";")
+    {
+        name.push_back(tempName);
+    }
 }
 
-void Name_pairs::read_ages(vector<double> ages)
+void Name_pairs::read_ages()
 {
+    //Prompts user for an age for each name
+    for(int i = 0; i < name.size(); i++)
+    {
+        cout << "Enter a corresponding age: ";
 
+        int tempAge;
+        cin >> tempAge;
+        age.push_back(tempAge);
+    }
 }
 
 void Name_pairs::print(const Name_pairs& pairs)
 {
+    //prints out name, age pairs in the order of the name vector (one per line)
 
 }
 
 void Name_pairs::sort(Name_pairs& pairs)
 {
-    
+    //Sort name vector in alpha order, then reorganizes age vector to match
+
 }
