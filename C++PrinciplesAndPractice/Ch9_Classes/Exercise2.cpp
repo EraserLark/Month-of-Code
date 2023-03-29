00bef8facf6e25fd11c692d625664584c14953ea
Conner A <eraserlark@gmail.com>
2023-03-29 11:13:23 -0500

    Ch9 Exercise2

---
+++ C++PrinciplesAndPractice/Ch9_Classes/Exercise2.cpp
@@ -0,0 +1,39 @@
+#include "../Libraries/std_lib_facilities.h"
+
+class Name_pairs
+{
+    vector<string> name;
+    vector<double> age;
+
+    void read_names(vector<string> names);
+    void read_ages(vector<double> ages);
+
+    void print(const Name_pairs& pairs);
+    void sort(Name_pairs& pairs);
+};
+
+int main()
+{
+
+    return 0;
+}
+
+void Name_pairs::read_names(vector<string> names)
+{
+
+}
+
+void Name_pairs::read_ages(vector<double> ages)
+{
+
+}
+
+void Name_pairs::print(const Name_pairs& pairs)
+{
+
+}
+
+void Name_pairs::sort(Name_pairs& pairs)
+{
+    
+}
