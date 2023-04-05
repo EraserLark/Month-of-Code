f41c6832af5d51ff4df5210759624b83fefad1a9
Conner A <eraserlark@gmail.com>
2023-04-05 13:12:19 -0500

    Reverse String

--- DataStructures_Algorithms/Stacks/StackReverseString.cpp
+++ DataStructures_Algorithms/Stacks/StackReverseString.cpp
@@ -1,8 +1,29 @@
+#include <cstring>
 #include <iostream>
-
+#include <stack>        //stack from the Standard Template Library (STL)
 using namespace std;
+
+void Reverse(char *C, int n);
 
 int main()
 {
-    
+    char C[] {'H','e','l','l','o'};
+    Reverse(C, 5);
+    cout << C;
+}
+
+void Reverse(char *C, int n)
+{
+    stack<char> s;
+
+    for(int i = 0; i < n; i++)
+    {
+        s.push(C[i]);
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        C[i] = s.top();
+        s.pop();
+    }
 }
