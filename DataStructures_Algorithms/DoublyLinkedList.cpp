81e5797ac13f80d29bbe2c5ca6da888540631c3b
Conner A <eraserlark@gmail.com>
2023-04-05 10:17:22 -0500

    print functions

--- DataStructures_Algorithms/DoublyLinkedList.cpp
+++ DataStructures_Algorithms/DoublyLinkedList.cpp
@@ -19,7 +19,11 @@ Node* head;
 
 int main()
 {
-    
+    InsertAtHead(4);
+    InsertAtHead(3);
+    InsertAtHead(7);
+    Print();
+    ReversePrint();
 }
 
 Node* GetNewNode(int x)
@@ -44,4 +48,36 @@ void InsertAtHead(int x)
     head->prev = temp;
     temp->next = head;
     head = temp;
+}
+
+void ReversePrint()
+{
+    Node* temp = head;
+    if(temp == NULL) return;    //empty list
+
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    while(temp != NULL)
+    {
+        cout << temp->data;
+        temp = temp->prev;
+    }
+
+    cout << endl;
+}
+
+void Print()
+{
+    Node* temp = head;
+
+    while(temp != NULL)
+    {
+        cout << temp->data;
+        temp = temp->next;
+    }
+
+    cout << endl;
 }
