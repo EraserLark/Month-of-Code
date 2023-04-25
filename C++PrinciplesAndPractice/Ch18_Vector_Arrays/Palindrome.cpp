11318324062f3a41853efaaed2ff448b0e706bca
Conner A <eraserlark@gmail.com>
2023-04-24 19:18:56 -0500

    Ch 18 - Palindrome with arrays

--- C++PrinciplesAndPractice/Ch18_Vector_Arrays/Palindrome.cpp
+++ C++PrinciplesAndPractice/Ch18_Vector_Arrays/Palindrome.cpp
@@ -3,21 +3,23 @@
 using namespace std;
 
 bool is_palindrome(const string& s);
+istream& read_word(istream& is, char* buffer, int max);
 
 int main()
 {
-    for(string s; cin >> s;)
+    constexpr int max = 128;
+    for(char s[max]; read_word(cin, s, max);)
     {
         cout << s << " is";
-        if(!is_palindrome(s) )cout << " not";
+        if(!is_palindrome(s, strlen(s)))cout << " not";
         cout << " a palindrome.\n";
     }
 }
 
-bool is_palindrome(const string& s)
+bool is_palindrome(const string& s, int n)
 {
     int first = 0;
-    int last = s.length() - 1;
+    int last = n - 1;
     while(first < last)
     {
         if(s[first] != s[last]) return false;
@@ -25,4 +27,11 @@ bool is_palindrome(const string& s)
         last--;
     }
     return true;
+}
+
+istream& read_word(istream& is, char* buffer, int max)
+{
+    is.width(max);  //Prevents buffer overflow for following operations
+    is >> buffer;
+    return is;
 }
